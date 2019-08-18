715dc31735cffe5b38a1d3f4485e46839341fcbc
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-08-18 13:11:32 +0100

    Updated Travis to run test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,10 @@ import logging
 from os import path as os_path
 from sys import path as sys_path
 
-from mafiapm import app
 from pytest import fixture
 
-
 sys_path.insert(0, os_path.join(os_path.dirname(os_path.abspath(__file__)), ".."))
+from mafiapm import app
 
 
 @fixture
